d552bef8cfaa5c6111b55bc4358db857072d37b2
mins00kwon <mskwon0918@gmail.com>
2025-04-21 21:25:29 +0900

    프로젝트 생성 및 .gitignore 추가. 테스트 커넥션 확인

---
+++ my-local-diary/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Health Check</h1>
+  <button @click="healthCheck">건강하니?</button>
+  <h3>{{ health }}</h3>
+</template>
+
+<script setup>
+  import { ref} from 'vue';
+
+  const health=ref("chcking....");
+
+  const healthCheck=async()=>{
+    const response=await fetch('http://localhost:8080/');
+    const text=await response.text();
+    health.value=text;
+  }
+  
+</script>
+
+
+<style scoped>
+</style>
