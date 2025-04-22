561d76296e1d182fec97af47af90896130f167ae
mins00kwon <mskwon0918@gmail.com>
2025-04-22 12:41:16 +0900

    jpa 작동 테스트

--- my-local-diary/src/App.vue
+++ my-local-diary/src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <h1>Health Check</h1>
-  <button @click="healthCheck">건강하니?</button>
+  <button @click="mybatisTest">Mybatis Test</button><br>
   <h3>{{ health }}</h3>
+  <hr>
+  <input type="text" v-model="inputValue" placeholder="값을 입력하세요" />
+  <button @click="jpaTest">JPA Test</button><br>
+  <h3>{{ jpaHealth }}</h3>
+  
 </template>
 
 <script setup>
   import { ref} from 'vue';
 
-  const health=ref("chcking....");
+  const health=ref("checking....");
+  const jpaHealth=ref("waiting for input...");
+  const inputValue=ref("");
 
-  const healthCheck=async()=>{
+  const mybatisTest=async()=>{
     const response=await fetch('http://localhost:8080/');
     const text=await response.text();
     health.value=text;
+  }
+
+  const jpaTest=async ()=>{
+    const response=await fetch('http://localhost:8080/update',{
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({value:inputValue.value})
+    });
+
+    const data=await response.text();
+    jpaHealth.value=data;
   }
   
 </script>
