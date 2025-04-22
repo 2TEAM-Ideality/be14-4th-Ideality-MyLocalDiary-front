76c1525aea4d2363cc54f9070d1ac46f33b9acf1
Hailyee <a11roundworld@naver.com>
2025-04-22 22:14:36 +0900

    feat sidebar 수정완

--- my-local-diary/src/components/common/Sidebar.vue
+++ my-local-diary/src/components/common/Sidebar.vue
@@ -2,7 +2,7 @@
     <VNavigationDrawer
       v-model="drawer"
       app
-      :width="isHover ? 200 : 60"
+      :width="isHover ? 200 : 80"
       permanent
       class="pa-2"
       @mouseenter="isHover = true"
@@ -13,48 +13,84 @@
         <v-list-item>
           <v-img
             v-if="!isHover"
-            src="@/assets/cursor_ 10.svg"
+            src="/src/assets/cursor_ 10.svg"
             alt="cursor"
-            width="36"
-            class="ml-n2"
+            
           ></v-img>
           <v-img
             v-else
-            src="@/assets/My_Local_Diary.png"
+            src="/src/assets/My_Local_Diary.png"
             alt="diary"
             width="150"
           ></v-img>
         </v-list-item>
   
         <v-list-item>
-          <v-img src="@/assets/Map.png" alt="map" width="24" class="mr-2" />
-          <span v-if="isHover">지도 홈</span>
-        </v-list-item>
-  
-        <v-list-item @click="goToMypage" class="clickable">
-          <v-img src="@/assets/person.png" alt="마이페이지" width="36" class="mr-2" />
-          <span v-if="isHover">마이페이지</span>
-        </v-list-item>
-  
-        <v-list-item>
-          <v-img src="@/assets/Pen.png" alt="pen" width="24" class="mr-2" />
-          <span v-if="isHover">글쓰기</span>
-        </v-list-item>
-  
-        <v-list-item>
-          <v-img src="@/assets/Search.png" alt="search" width="24" class="mr-2" />
-          <span v-if="isHover">검색</span>
-        </v-list-item>
-  
-        <v-list-item>
-          <v-img src="@/assets/stamp.png" alt="stamp" width="36" class="mr-2 ml-n1" />
-          <span v-if="isHover">스탬프 목록</span>
-        </v-list-item>
-  
-        <v-list-item>
-          <v-img src="@/assets/notifications.png" alt="notifications" width="24" class="mr-2" />
-          <span v-if="isHover">알림</span>
-        </v-list-item>
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/Map.png"
+      alt="map"
+      class="menu-icon"
+    />
+    <span v-if="isHover">지도 홈</span>
+  </div>
+</v-list-item>
+
+<v-list-item @click="goToMypage" class="clickable">
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/person.png"
+      alt="마이페이지"
+      class="menu-icon"
+    />
+    <span v-if="isHover">마이페이지</span>
+  </div>
+</v-list-item>
+
+<v-list-item>
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/Pen.png"
+      alt="pen"
+      class="menu-icon"
+    />
+    <span v-if="isHover">글쓰기</span>
+  </div>
+</v-list-item>
+
+<v-list-item>
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/Search.png"
+      alt="search"
+      class="menu-icon"
+    />
+    <span v-if="isHover">검색</span>
+  </div>
+</v-list-item>
+
+<v-list-item>
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/stamp.png"
+      alt="stamp"
+      class="stamp-icon ml-n1"
+    />
+    <span v-if="isHover">스탬프 목록</span>
+  </div>
+</v-list-item>
+
+<v-list-item>
+  <div class="menu-item">
+    <v-img
+      src="/src/assets/notifications.png"
+      alt="notifications"
+      class="menu-icon"
+    />
+    <span v-if="isHover">알림</span>
+  </div>
+</v-list-item>
+
       </v-list>
     </VNavigationDrawer>
   </template>
@@ -73,8 +109,29 @@
   </script>
   
   <style scoped>
-  .clickable {
-    cursor: pointer;
+  .menu-item {
+    display: flex;
+    align-items: center;
+  }
+  
+  .menu-icon {
+    width: 25px;
+    height: 25px;
+    min-width: 25px;
+    max-width: 25px;
+    flex-shrink: 0;
+    margin-right: 8px;
+    object-fit: contain;
+  }
+
+  .stamp-icon {
+    width: 35px;
+    height: 35px;
+    min-width: 35px;
+    max-width: 35px;
+    flex-shrink: 0;
+    margin-right: 8px;
+    object-fit: contain;
   }
   </style>
   
