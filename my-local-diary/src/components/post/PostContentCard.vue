9826a8eed4b66e1ff85f16399204f9958023622a
swjang7269 <azamiyuko7269@gmail.com>
2025-04-25 15:33:46 +0900

    feat 게시글 본문 컴포넌트 구현

---
+++ my-local-diary/src/components/post/PostContentCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card flat class="pa-3">
+    <div class="text-body-1" style="white-space: pre-line">{{ content }}</div>
+
+  </v-card>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+  defineProps({
+    postType: String,
+    content: String,
+  })
+  const postTypeLabel = computed(() => {
+    return postType === 'post' ? 'Post' : 'Diary'
+  })
+</script>
