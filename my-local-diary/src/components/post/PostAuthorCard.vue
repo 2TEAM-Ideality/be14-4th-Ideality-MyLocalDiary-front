d49e8ebe7dd23809fee34aa361641b958cd78070
swjang7269 <azamiyuko7269@gmail.com>
2025-04-25 16:39:39 +0900

    refactor 게시글에 장소 정보 추가

--- my-local-diary/src/components/post/PostAuthorCard.vue
+++ my-local-diary/src/components/post/PostAuthorCard.vue
@@ -4,21 +4,31 @@
         <img :src="author.avatar" alt="Avatar" />
         </v-avatar>
         <div class="ml-3">
-        <div class="font-weight-medium">{{ author.name }}</div>
-        <div class="text-grey text-caption d-flex align-center">
-            <v-icon size="16" class="mr-1">mdi-map-marker</v-icon>
-        </div>
+            <div class="font-weight-medium">{{ author.name }}</div>
+            <div class="neighborhoods d-flex flex-wrap gap-2">
+                <LocationChip
+                    v-for="(place, i) in neighborhoods"
+                    :key="i"
+                    :place="place"
+                />
+                <v-chip size="small" class="muted" color="grey">+ 6</v-chip>
+            </div>
         </div>
     </v-card>
 </template>
 
 <script setup>
+    import LocationChip from '@/components/common/LocationChip.vue'
     defineProps({
         author: {
             type: Object,
             required: true,
         },
     })
+    const neighborhoods = [
+    { name: '신대방동 294', latitude: 37.4854, longitude: 126.9016 },
+    { name: '보라매로 73', latitude: 37.4923, longitude: 126.9248 }
+  ]
 </script>
 
 <style scoped>
