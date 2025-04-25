8f36fd98f5cbfc009d5322e146944f72665eeae5
swjang7269 <azamiyuko7269@gmail.com>
2025-04-25 15:34:09 +0900

    feat 게시글 작성자 정보 컴포넌트 구현

---
+++ my-local-diary/src/components/post/PostAuthorCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card flat class="d-flex align-center mb-2">
+        <v-avatar size="40">
+        <img :src="author.avatar" alt="Avatar" />
+        </v-avatar>
+        <div class="ml-3">
+        <div class="font-weight-medium">{{ author.name }}</div>
+        <div class="text-grey text-caption d-flex align-center">
+            <v-icon size="16" class="mr-1">mdi-map-marker</v-icon>
+        </div>
+        </div>
+    </v-card>
+</template>
+
+<script setup>
+    defineProps({
+        author: {
+            type: Object,
+            required: true,
+        },
+    })
+</script>
+
+<style scoped>
+
+</style>
