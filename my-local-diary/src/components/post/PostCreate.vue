2c6ed28fd0d1128d5dc00b9086011cec3f3931be
minsun24 <jms49526@gmail.com>
2025-05-02 03:06:12 +0900

    fix 나머지

--- my-local-diary/src/components/post/PostCreate.vue
+++ my-local-diary/src/components/post/PostCreate.vue
@@ -135,7 +135,6 @@ import { Pagination } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import CustomMarker from '@/components/common/CustomMarker.vue'
-import { useUserStore } from '@/stores/userStore'
 
 const router = useRouter()
 const userStore = useUserStore()
